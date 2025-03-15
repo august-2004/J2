69c6adc2e654e88942e6b179fc509844ec9749a7
august-2004 <115717134+august-2004@users.noreply.github.com>
2025-03-15 16:42:34 +0530

    cors issues 2

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 app.use(
 	cors({
-		origin: "https://theskribe.vercel.app", // Single correct URL
+		origin: "*",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
